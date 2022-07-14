5e06bd2331d5b9b83a371970f0fc77f1f8fe2a8a
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-14 12:52:13 -0500

    Main Actualizado

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { API_PUERTO_MSO_TRANSACCIONES, API_SERVIDOR_MSO_TRANSACCIONES } = process.env;
+
+  const mso: MicroserviceOptions = {
+    transport: Transport.TCP,
+    options: {
+      host: API_SERVIDOR_MSO_TRANSACCIONES,
+      port: +API_PUERTO_MSO_TRANSACCIONES
+    }
+  }
+
+  const app = await NestFactory.createMicroservice(AppModule, mso);
+  await app.listen();
 }
 bootstrap();
