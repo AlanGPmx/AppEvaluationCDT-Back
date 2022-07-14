b3c1d1c30b56f35a035eeb004c2095dd462efc24
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-14 15:17:05 -0500

    feat: ProductosModule gregado

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductosModule } from './components/productos/productos.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ProductosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
