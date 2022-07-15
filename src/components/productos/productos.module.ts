fb57dbf540a778b0bf917de29f191b2d737844c9
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-15 14:51:45 -0500

    fix: Se importa el TypeOrmModule for Feature

--- src/components/productos/productos.module.ts
+++ src/components/productos/productos.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductosService } from './productos.service';
 import { ProductosController } from './productos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entity/product-entity';
 
 @Module({
   providers: [ProductosService],
-  controllers: [ProductosController]
+  controllers: [ProductosController],
+  imports: [ TypeOrmModule.forFeature([Product]) ]
 })
 export class ProductosModule {}
