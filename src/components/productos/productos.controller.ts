aebfca6763500e0bd9c315c7c00c7e7851223feb
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-15 14:51:13 -0500

    Se agrega el get de obtenerProductos

--- src/components/productos/productos.controller.ts
+++ src/components/productos/productos.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { Respuesta } from './interfaces/respuesta.interface';
+import { ProductosService } from './productos.service';
 
 @Controller('productos')
-export class ProductosController {}
+export class ProductosController {
+  constructor(private readonly productosService: ProductosService) {}
+
+  // @Get()
+  @MessagePattern('obtenerProductos')
+  async obtenerProductos(): Promise<Respuesta> {
+    return await this.productosService.obtenerProductos();
+  }
+}
