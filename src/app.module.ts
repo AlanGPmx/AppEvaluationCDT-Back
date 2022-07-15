02f61b16300bde2348cbd05a17ec85db3f38e0de
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-15 14:50:38 -0500

    Se mueve el TypeORMModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './components/productos/entity/product-entity';
 import { ProductosModule } from './components/productos/productos.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env['BD_SERVIDOR'],
+      port: 3306,
+      username: process.env['BD_USUARIO'],
+      password: process.env['BD_CLAVE'],
+      database: process.env['BD_NOMBRE'],
+      entities: [Product],
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
     ProductosModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
